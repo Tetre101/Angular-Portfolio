92c2d9d3d6e87e0df74fd9c8f325ae7651395042
Techlead <adesanya1ademola@gmail.com>
2023-02-20 22:01:08 +0100

    Fixed up

--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -5,25 +5,35 @@ import { Component } from '@angular/core';
   templateUrl: './project.component.html',
   styleUrls: ['./project.component.scss'],
 })
-export class ProjectComponent {}
+export class ProjectComponent {
 
-interface Project {
-  name: string;
-  link: string;
-  'Github-link': string;
-  description: string;
-}
-let myProject: Project[] = [
+// class Project {
+//   name: string;
+//   link: string;
+//   githubLink: string;
+//   description: string;
+// }
+
+myProject = [
   {
     name: 'Guess-A-number-Js-Game',
     link: 'https://tetre101.github.io/Guess-A-number-Js-Game/',
-    'Github-link': 'https://github.com/Tetre101/Guess-A-number-Js-Game',
+    githubLink: 'https://github.com/Tetre101/Guess-A-number-Js-Game',
     description: 'Guess a number game using javascript',
   },
   {
     name: 'Roll-A-Dice-Game',
     link: 'https://tetre101.github.io/Roll-A-Dice-Game/',
-    'Github-link': 'https://github.com/Tetre101/Roll-A-Dice-Game',
+    githubLink: 'https://github.com/Tetre101/Roll-A-Dice-Game',
     description: 'Roll a dice game using javascript',
   },
 ];
+
+notify = () => {
+  alert('The page will load shortly')
+}
+
+ngOnInit() {
+  this.notify()
+}
+}
