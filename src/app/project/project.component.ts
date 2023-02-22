e9ba730b2672b8bed2f9a397b4706148c132766f
Tetre101 <basilpeter989@gmail.com>
2023-02-22 14:42:26 +0100

    created the project page and styles

--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -1,39 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+export interface Project {
+  name: string;
+  link: string;
+  githubLink: string;
+  description: string;
+  image: string;
+}
 
 @Component({
-  selector: 'app-project',
+  selector: 'app-myproject',
   templateUrl: './project.component.html',
   styleUrls: ['./project.component.scss'],
 })
-export class ProjectComponent {
+export class ProjectComponent implements OnInit {
+  myProject: Project[] = [
+    {
+      name: 'Guess-A-number-Js-Game',
+      link: 'https://tetre101.github.io/Guess-A-number-Js-Game/',
+      githubLink: 'https://github.com/Tetre101/Guess-A-number-Js-Game',
+      description: 'Guess a number game using javascript',
+      image: 'assets/guess.png',
+    },
+    {
+      name: 'Roll-A-Dice-Game',
+      link: 'https://tetre101.github.io/Roll-A-Dice-Game/',
+      githubLink: 'https://github.com/Tetre101/Roll-A-Dice-Game',
+      description: 'Roll a dice game using javascript',
+      image: 'assets/dice.png',
+    },
+  ];
 
-// class Project {
-//   name: string;
-//   link: string;
-//   githubLink: string;
-//   description: string;
+  constructor() {}
+
+  ngOnInit() {}
+
+  notify() {
+    alert('You clicked the View button!');
+  }
+}
+
+// @Component({
+//   selector: 'app-project',
+//   templateUrl: './project.component.html',
+//   styleUrls: ['./project.component.scss'],
+// })
+// export class ProjectComponent {
+//   myProject = [
+//     {
+//       name: 'Guess-A-number-Js-Game',
+//       link: 'https://tetre101.github.io/Guess-A-number-Js-Game/',
+//       githubLink: 'https://github.com/Tetre101/Guess-A-number-Js-Game',
+//       description: 'Guess a number game using javascript',
+//     },
+//     {
+//       name: 'Roll-A-Dice-Game',
+//       link: 'https://tetre101.github.io/Roll-A-Dice-Game/',
+//       githubLink: 'https://github.com/Tetre101/Roll-A-Dice-Game',
+//       description: 'Roll a dice game using javascript',
+//     },
+//   ];
+
+//   notify = () => {
+//     alert('The page will load shortly');
+//   };
+
+//   ngOnInit() {
+//     this.notify();
+//   }
 // }
-
-myProject = [
-  {
-    name: 'Guess-A-number-Js-Game',
-    link: 'https://tetre101.github.io/Guess-A-number-Js-Game/',
-    githubLink: 'https://github.com/Tetre101/Guess-A-number-Js-Game',
-    description: 'Guess a number game using javascript',
-  },
-  {
-    name: 'Roll-A-Dice-Game',
-    link: 'https://tetre101.github.io/Roll-A-Dice-Game/',
-    githubLink: 'https://github.com/Tetre101/Roll-A-Dice-Game',
-    description: 'Roll a dice game using javascript',
-  },
-];
-
-notify = () => {
-  alert('The page will load shortly')
-}
-
-ngOnInit() {
-  this.notify()
-}
-}
